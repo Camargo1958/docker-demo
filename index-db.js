dfae66968ffc570f0038b536b343e19b51b2ef47
Camargo1958 <aldrov_camargo@hotmail.com>
2022-05-22 21:50:22 -0300

    updates

--- index-db.js
+++ index-db.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mysql = require("mysql");
-var con = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.env.MYSQL_USER, password: process.env.MYSQL_PASSWORD, database: process.env.MYSQL_DATABASE});
+var con = mysql.createConnection({host: process.env.MYSQL_HOST, user: process.env.MYSQL_USER, password: process.env.MYSQL_PASSWORD, database: process.env.MYSQL_DATABASE});
 
 // mysql code
 
